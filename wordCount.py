20f563073e4c514ff295d3bec8104ed13bef9931
nicoletorres <nicolevardo@gmail.com>
2020-09-01 00:05:56 -0600

    read file and for loop for word count

--- wordCount.py
+++ wordCount.py
@@ -13,19 +13,28 @@ import sys
 inputFname = sys.argv[1]
 outputFname = sys.argv[2]
 
-def main():
     # how to read a file
     # takes as input the name of an input file and output file
     file = open(inputFname, "r")
+    x = file.readline()
+    print(x)
+
+# initialize dictionary
+    dictionary = {}
+
+# count. keeps track of the total the number of times each word occurs in the text file
+    for word in x:
+        if word not in dictionary:
+            dictionary[word] = 0
+        dictionary[word] += 1
+
+# excluding white space and punctuation and ignores upper/lower case
 
 
-# excluding white space and punctuation is case-insensitive
-
-
-#keeps track of the total the number of times each word occurs in the text file
 
 
 #print out to the output file (overwriting if it exists) the list of words
 
 #sorted in descending order with their respective totals separated by a space, one word per line
 
+def main():
