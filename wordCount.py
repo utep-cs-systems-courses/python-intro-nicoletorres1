13acfa2152916869d2cd239337fc6784e83a1834
nicoletorres <nicolevardo@gmail.com>
2020-08-31 21:20:17 -0600

    created new python file

---
+++ wordCount.py
@@ -0,0 +1,10 @@
+
+
+#takes as input the name of an input file and output file
+
+#keeps track of the total the number of times each word occurs in the text file
+#excluding white space and punctuation
+
+#is case-insensitive
+#print out to the output file (overwriting if it exists) the list of words sorted in descending
+#order with their respective totals separated by a space, one word per line
