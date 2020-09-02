d25f8b4eb6fa94c201db15f9d2243cb2a84b35f0
nicoletorres <nicolevardo@gmail.com>
2020-09-01 22:45:43 -0600

    take out special characters and spaces

--- wordCount.py
+++ wordCount.py
@@ -8,33 +8,55 @@
 import re
 import os
 import sys
+#import Counter
 
 #contains the command-line arguments passed
 inputFname = sys.argv[1]
 outputFname = sys.argv[2]
 
-    # how to read a file
-    # takes as input the name of an input file and output file
-    file = open(inputFname, "r")
-    x = file.readline()
-    print(x)
+# how to read a file
+# takes as input the name of an input file and output file
+# excluding white space and punctuation and ignores upper/lower case
+file = open(inputFname, "r")
+x = file.readlines()
+# make a new list of words
+biggerlist = []
+for word in x:
+    word = word.lower()
+    word = word.replace("?", "")
+    word = word.replace(".", "")
+    word = word.replace(":", "")
+    word = word.replace(",", "")
+    word = word.replace(";", "")
+    word = word.replace("&", "")
+    word = word.replace("-", " ")
+    word = word.replace("'", " ")
+    biggerlist.append(word.split())
+#print(biggerlist)
 
 # initialize dictionary
-    dictionary = {}
+dictionary = {}
 
 # count. keeps track of the total the number of times each word occurs in the text file
-    for word in x:
+for smallerlist in biggerlist:
+    for word in smallerlist:
+
         if word not in dictionary:
-            dictionary[word] = 0
-        dictionary[word] += 1
-
-# excluding white space and punctuation and ignores upper/lower case
-
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
 
 
+# print out to the output file (overwriting if it exists) the list of words
+# sorted in descending order with their respective totals separated by a space, one word per line
+print(dictionary)
 
-#print out to the output file (overwriting if it exists) the list of words
+with open(outputFname, 'w') as outputFile:
+    for i in sorted(dictionary):
+        outputFile.write(i + " " + str(dictionary[i]) + "\n")
 
-#sorted in descending order with their respective totals separated by a space, one word per line
 
-def main():
+
+
+#def __main__():
+    #pass
